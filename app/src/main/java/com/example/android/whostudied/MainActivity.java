4f8fb51f0dc1c2ec0d9a6908d920ba215da08e0a
osemars <osemars@github.com>
2018-06-28 10:59:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/whostudied/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.android.whostudied;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaration of the container
+    public static final String EXTRA_MESSAGE =
+            //location of the container
+            "com.example.android.whostudied.extra.MESSAGE";
+    //initiate numberOfSubjects to evaluate
+    //numberOfSubjects that would be displayed in test page
+    int numberOfSubjects = 0;
+    //Chant for Male or Female
+    String chant = "";
+    Boolean NigeriaBoxChecked = false;
+    Boolean FounderBoxChecked = false;
+    Boolean StartupBoxChecked = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void radio_click(View view) {
+        //maleRBtn is changed from View to RadioButton and stored in MaleRadio
+        RadioButton MaleRadio = findViewById(R.id.maleRBtn);
+        Boolean MaleButtonSelected = MaleRadio.isChecked();
+        TextView boysHail = findViewById(R.id.boys_house);
+
+        //femaLeRBtn is changed from View to RadioButton and stored in FemaleRadio
+        RadioButton FemaleRadio = findViewById(R.id.femaleRBtn);
+        Boolean FemaleButtonSelected = FemaleRadio.isChecked();
+        TextView girlsHail = findViewById(R.id.girls_house);
+
+        //When "Boys" is selected
+        if (MaleButtonSelected) {
+            boysHail.setTextSize(12);
+            chant = "for the guys";
+        } else boysHail.setTextSize(0);
+        //When "Girls" is selected
+        if (FemaleButtonSelected) {
+            girlsHail.setTextSize(12);
+            chant = "for the babes";
+        } else girlsHail.setTextSize(0);
+    }
+
+    //When you check Nigeria
+    public void onCheckboxNigeria(View view) {
+        numberOfSubjects = numberOfSubjects + 1;
+        if (numberOfSubjects >= 4) {//number of subjects cannot be more than 3
+            return;
+        }
+        CheckBox nigeriaBox = findViewById(R.id.nigeria);
+        NigeriaBoxChecked = nigeriaBox.isChecked();
+        if (NigeriaBoxChecked) {
+            nigeriaBox.setChecked(true);
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, "You will answer " + numberOfSubjects * 2 + " questions", duration);
+            toast.show();
+        }
+    }
+
+    //When you check Founders
+    public void onCheckboxFounders(View view) {
+        numberOfSubjects = numberOfSubjects + 1;
+        if (numberOfSubjects >= 4) {//number of subjects cannot be more than 3
+            return;
+        }
+        CheckBox founderBox = findViewById(R.id.founders);
+        FounderBoxChecked = founderBox.isChecked();
+        if (FounderBoxChecked) {
+            founderBox.setChecked(true);
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, "You will answer " + numberOfSubjects * 2 + " questions", duration);
+            toast.show();
+        }
+    }
+
+    //When you check Startups
+    public void onCheckboxStartups(View view) {
+        numberOfSubjects = numberOfSubjects + 1;
+        if (numberOfSubjects >= 4) {//number of subjects cannot be more than 3
+            return;
+        }
+        CheckBox startupBox = findViewById(R.id.startups);
+        StartupBoxChecked = startupBox.isChecked();
+        if (StartupBoxChecked) {
+            startupBox.setChecked(true);
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, "You will answer " + numberOfSubjects * 2 + " questions", duration);
+            toast.show();
+        }
+    }
+
+    //when begin button is clicked
+    public void startTest(View view) {
+        //logic for checking which questions to display on Test Activity
+        if (NigeriaBoxChecked) {
+            NigeriaBoxChecked = true;
+        }
+        if (FounderBoxChecked) {
+            FounderBoxChecked = true;
+        }
+        if (StartupBoxChecked) {
+            StartupBoxChecked = true;
+        }
+
+        // Get student's name
+        //Change from View to EditText
+        EditText nameField = findViewById(R.id.name_input);
+        //save the Editable text in nameEditable variable
+        Editable nameEditable = nameField.getText();
+        //Convert to String type and store in name variable
+        String name = nameEditable.toString();
+        //Declare and Initiate a new Intent object
+        Intent intent = new Intent(this, TestPage.class);
+        //Adding data to the Intent, to be transferred to Test Activity
+        intent.putExtra(EXTRA_MESSAGE, name);
+        intent.putExtra(Intent.EXTRA_TEXT, chant);
+        intent.putExtra(Intent.EXTRA_RESULT_RECEIVER, NigeriaBoxChecked);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, FounderBoxChecked);
+        intent.putExtra(Intent.EXTRA_PROCESS_TEXT, StartupBoxChecked);
+        //launch Test Activity
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/android/whostudied/TestPage.java
@@ -0,0 +1,189 @@
+package com.example.android.whostudied;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.android.whostudied.MainActivity.EXTRA_MESSAGE;
+
+public class TestPage extends AppCompatActivity {
+    //chant: check MainActivity, Line49 & 54
+    String chant = "";
+    Boolean NigeriaBoxChecked;
+    Boolean FounderBoxChecked;
+    Boolean StartupBoxChecked;
+    int a = 0;
+    int b = 0;
+    int c = 0; //a,b,c are the number of questions to be displayed.
+    int OVERALL_SCORE;
+    Boolean moha_checked = false;
+    Boolean bubu_checked = false;
+    Boolean koko_checked = false;
+    Boolean musa_checked = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_page);
+        //Receiving data from MainActivity to TestPage.java
+        //Receive name and sex of user, and store in value and chant
+        Intent intent = getIntent();
+        String value = getIntent().getStringExtra(EXTRA_MESSAGE);
+        //Change the View where "name" should be displayed into a TextView and store in textView
+        String value2 = getIntent().getStringExtra(intent.EXTRA_TEXT);
+        Boolean value3 = getIntent().getBooleanExtra(intent.EXTRA_RESULT_RECEIVER, false);
+        Boolean value4 = getIntent().getBooleanExtra(intent.EXTRA_ALLOW_MULTIPLE, false);
+        Boolean value5 = getIntent().getBooleanExtra(intent.EXTRA_PROCESS_TEXT, false);
+        TextView textView = findViewById(R.id.name_output);
+        //add name to show in textView
+        textView.setText(value + ", let's see how much you really know!");
+        textView.setTextColor(Color.RED);
+        // assign value2 to chant
+        chant = value2;
+        NigeriaBoxChecked = value3;
+        FounderBoxChecked = value4;
+        StartupBoxChecked = value5;
+        //setting visibility according to the subject(s) you choose in MainActivity
+        //setting visibility for questions about Nigeria
+        if (value3 == true) {
+            findViewById(R.id.Question_one).setVisibility(View.VISIBLE);
+            findViewById(R.id.cb_musa).setVisibility(View.VISIBLE);
+            findViewById(R.id.cb_mohammed).setVisibility(View.VISIBLE);
+            findViewById(R.id.cb_buhari).setVisibility(View.VISIBLE);
+            findViewById(R.id.cb_aliko).setVisibility(View.VISIBLE);
+            findViewById(R.id.Question_two).setVisibility(View.VISIBLE);
+            findViewById(R.id.FreeText).setVisibility(View.VISIBLE);
+            a = 2;
+        } else a = 0;
+        //setting visibility for questions about Founders(Subject)
+        if (value4 == true) {
+            findViewById(R.id.Question_three).setVisibility(View.VISIBLE);
+            findViewById(R.id.options3).setVisibility(View.VISIBLE);
+            findViewById(R.id.Question_four).setVisibility(View.VISIBLE);
+            findViewById(R.id.option4).setVisibility(View.VISIBLE);
+            b = 2;
+        } else b = 0;
+        //setting visibility for questions about Startups(Subject)
+        if (value5 == true) {
+            findViewById(R.id.Question_5).setVisibility(View.VISIBLE);
+            findViewById(R.id.options5).setVisibility(View.VISIBLE);
+            findViewById(R.id.Question_6).setVisibility(View.VISIBLE);
+            findViewById(R.id.option6).setVisibility(View.VISIBLE);
+            c = 2;
+        } else c = 0;
+    }
+
+    //onCheck methods for the Checkboxes for number1 question in Nigeria(Subject)
+    public void onCheckboxmohammed(View view) {
+        CheckBox mohamoha = findViewById(R.id.cb_mohammed);
+        moha_checked = mohamoha.isChecked();
+    }
+
+    public void onCheckboxBuhari(View view) {
+        CheckBox bubu = findViewById(R.id.cb_buhari);
+        bubu_checked = bubu.isChecked();
+    }
+
+    public void alikoClick(View view) {
+        CheckBox alikoko = findViewById(R.id.cb_aliko);
+        koko_checked = alikoko.isChecked();
+    }
+
+    public void musaClick(View view) {
+        CheckBox musa = findViewById(R.id.cb_musa);
+        musa_checked = musa.isChecked();
+    }
+
+    //onClick for the submit button to submit test paper
+    public void finish(View view) {
+        //Evaluation the highest possible score according to the number of Subject picked
+        OVERALL_SCORE = a + b + c;
+        //Initializing congratulatory message to be displayed when scoring
+        String passed = "Congrats";
+        //initiating scores for each question
+        double score1a = 0;
+        double score1b = 0;
+        double score1c = 0;
+        double score1d = 0;
+        int score2 = 0;
+        int score3 = 0;
+        int score4 = 0;
+        int score5 = 0;
+        int score6 = 0;
+
+        //4 Checkboxes for question 1 in Nigeria(Subject)
+        if (moha_checked) {
+            score1a = 0.5;
+        } else score1a = 0;
+        if (bubu_checked) {
+            score1b = 0.5;
+        } else score1b = 0;
+        if (koko_checked) {
+            score1c = -0.5;
+        } else score1c = 0;
+        if (musa_checked) {
+            score1d = -0.5;
+        } else score1d = 0;
+
+        //EditText for question 2 in Nigeria(Subject)
+        //Answer is received, changed to String and stored in AnswerFree
+        EditText FreeAnswer = findViewById(R.id.FreeText);
+        String AnswerFree = FreeAnswer.getText().toString();
+        String ANSWER = "Abuja";
+        if (AnswerFree.equals(ANSWER)) {
+            score2 = 1;
+        } else score2 = 0;
+
+        //RadioButtons for question 1 in Founders(Subject)
+        RadioButton AnswerRadio3 = findViewById(R.id.option_3b);
+        Boolean Radio3Selected = AnswerRadio3.isChecked();
+        if (Radio3Selected) {
+            score3 = 1;
+        } else score3 = 0;
+        //RadioButtons for question 2 in Founders(Subject)
+        RadioButton AnswerRadio4 = findViewById(R.id.option_4a);
+        Boolean Radio4Selected = AnswerRadio4.isChecked();
+        if (Radio4Selected) {
+            score4 = 1;
+        } else score4 = 0;
+        //RadioButtons for question 1 in Startups(Subject)
+        RadioButton AnswerRadio5 = findViewById(R.id.option_5c);
+        Boolean Radio5Selected = AnswerRadio5.isChecked();
+        if (Radio5Selected) {
+            score5 = 1;
+        } else score5 = 0;
+        //RadioButtons for question 2 in Startups(Subject)
+        RadioButton AnswerRadio6 = findViewById(R.id.option_6a);
+        Boolean Radio6Selected = AnswerRadio6.isChecked();
+        if (Radio6Selected) {
+            score6 = 1;
+        } else score6 = 0;
+
+        //adding up the scores and converting to percentage
+        double score = score1a + score1b + score1c + score1d + score2 + score3 + score4 + score5 + score6;
+        double evaluate_score = score * 100;
+        double percent_score = evaluate_score / OVERALL_SCORE;
+        //if no Subject is picked, so as to display 0% as the percentage_score not NaN%.
+        if (OVERALL_SCORE == 0) {
+            percent_score = 0;
+        }
+        //Say sorry if score is less than 45% and congrats (Line109) if its more
+        if (percent_score < 45) {
+            passed = "Sorry";
+        }
+
+        //making a toast to display the final result
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, passed + "," + " you scored " + percent_score + "% " + chant, duration);
+        toast.show();
+    }
+}
